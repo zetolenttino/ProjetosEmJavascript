1524625820a3022cda1c1fbcc028bdf3f9e2590e
zetolenttinopc <zetolenttino@gmail.com>
2021-09-15 15:00:15 -0300

    verificação estrangeiro e ver multa

---
+++ condicoes2.js
@@ -0,0 +1,7 @@
+let pais = 'frança'
+console.log(`Vivendo em ${pais}`)
+if (pais != 'brasil') {
+    console.log('Você é estrangeiro!')
+} else {
+    console.log('Você é brasileiro!')
+}
---
+++ variaveis.js
@@ -0,0 +1,33 @@
+let idade = 2021 - 1998;
+console.log('idade', idade);
+let altura = 180;
+console.log('altura', altura);
+
+
+let a = 10
+let A = 20
+console.log('valor da variavel a', a)
+console.log('valor da variavel A', A)
+
+let n = 200
+console.log('tipo da variável n', typeof n)
+let m = "300"
+console.log('tipo da variável m', typeof m)
+
+let mInteger = Number.parseInt(m)
+console.log('tipo da variável mInteger', typeof mInteger)
+
+console.log("m+1", m + 1)
+console.log("+m+1", +m + 1)
+console.log("mInteger+1", mInteger + 1)
+
+/*  2+8/4 = 4 
+   (2+8)/4 = 2,5    parenteses da preferência para começar  conta*/
+
+
+let conta1 = 5 + 3;
+let conta2 = conta1 % 5;
+let conta3 = 5 * conta2 ** 2;
+let conta4 = 10 - conta1 / 2;
+let conta5 = 6 * 2 / conta4;
+let conta5 = conta2 % conta5 + 4 / conta5;
