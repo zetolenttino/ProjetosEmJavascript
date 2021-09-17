f82c54a402d2683f5506cb7642b5ddc937a262db
zetolenttinopc <zetolenttino@gmail.com>
2021-09-17 10:40:16 -0300

    publicando na internet

---
+++ verificadordeimgem/verificadoridade.js
@@ -0,0 +1,20 @@
+function verificar() {
+    let data = new Date()
+    let ano = data.getFullYear()
+    let fano = document.getElementById('txtano')
+    let res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        let fsex = document.getElementsByName('radsex')
+        let idade = ano - Number(fano.value)
+        let genero = ''
+        if (fsex[0].checked) {
+            genero = 'Homem'
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+        }
+        res.style
+        res.innerHTML = `Detectamos: ${genero} com ${idade} anos.`
+    }
+}
